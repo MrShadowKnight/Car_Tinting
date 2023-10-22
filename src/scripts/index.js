817afa118e8583581dc9a71274ab55f22e6f3f56
MrShadowKnight <volodymyr.pinchak@gmail.com>
2023-10-22 10:40:10 +0300

    Add.changes

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,3 +1,4 @@
+// start point
 document.addEventListener("DOMContentLoaded", () => {
   $(".hero__slider").slick({
     dots: true,
@@ -7,5 +8,9 @@ document.addEventListener("DOMContentLoaded", () => {
     slidesToShow: 1,
     autoplay: true,
     autoplaySpeed: 3000,
+  });
+  $(".gallery__slider").slick({
+    infinite: true,
+    slidesToShow: 4,
   });
 });
