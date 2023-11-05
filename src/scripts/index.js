598ba4579f89adf9054574e019a829765a453e88
MrShadowKnight <volodymyr.pinchak@gmail.com>
2023-11-05 14:15:00 +0200

    Add.changes

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,3 +1,10 @@
+const clearForm = (name, phone, email, message) => {
+  name.value = "";
+  phone.value = "";
+  email.value = "";
+  message.value = "";
+};
+
 // start point
 document.addEventListener("DOMContentLoaded", () => {
   $(".hero__slider").slick({
@@ -38,4 +45,43 @@ document.addEventListener("DOMContentLoaded", () => {
       jQuery(".burger__menu").toggleClass("opened");
     });
   });
+
+  // // Get element from do cument
+  // const FORM = document.getElementById(".form__content_form");
+  // const NAME = document.getElementById(".form__content_form_name");
+  // const PHONE = document.getElementById(".form__content_form_phone");
+  // const EMAIL = document.getElementById(".form__content_form_email");
+  // const MESSAGE = document.getElementById(".form__content_form_message");
+
+  // // console
+  // console.log(NAME.value);
+  // console.log(PHONE.value);
+  // console.log(EMAIL.value);
+  // console.log(MESSAGE.value);
+
+  // // add listemers
+  // FORM.addEventListener("submit", (e) => {
+  //   let frame = {
+  //     name: NAME.value,
+  //     phone: PHONE.value,
+  //     email: EMAIL.value,
+  //     message: MESSAGE.value,
+  //   };
+  //   console.log(frame);
+  //   clearForm(NAME, PHONE, EMAIL, MESSAGE);
+  // });
+  function submitForm() {
+    var name = document.getElementById(".form__content_form_name").value;
+    var phone = document.getElementById("age").value;
+    var email = document.getElementById("email").value;
+    var occupation = document.getElementById("occupation").value;
+
+    var result = "Ім'я: " + name + "<br>";
+    result += "Вік: " + phone + "<br>";
+    result += "Email: " + email + "<br>";
+    result += "Заняття: " + occupation + "<br>";
+
+    document.getElementById("result").innerHTML = result;
+}
+
 });
